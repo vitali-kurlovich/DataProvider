c680fa9f0e65783b218289ea0ddb3e77ad2fdf66
vitali-kurlovich <vitalikurlovich@gmail.com>
2024-12-05 22:14:46 +0400

    Logging

--- Sources/DataProvider/Parametred/URLSession/URLSessionProvider.swift
+++ Sources/DataProvider/Parametred/URLSession/URLSessionProvider.swift
@@ -8,9 +8,8 @@
 import Foundation
 import HTTPTypes
 import HTTPTypesFoundation
+import OSLog
 
-@available(iOS 13.0, *)
-@available(macOS 10.15, *)
 public struct URLSessionProvider: ParametredDataProvider {
     public typealias Result = (Data, HTTPResponse)
     public typealias Params = HTTPRequest
@@ -18,28 +17,59 @@ public struct URLSessionProvider: ParametredDataProvider {
     public typealias ProviderError = DataProviderError
 
     let urlSession: URLSession
+    let logger: Logger?
 
-    public init(urlSession: URLSession = .shared) {
+    let signposter: OSSignposter?
+
+    public init(urlSession: URLSession = .shared, logger: Logger? = nil, signposter: OSSignposter? = nil) {
         self.urlSession = urlSession
+        self.logger = logger
+        self.signposter = signposter
     }
 
     public func fetch(_ params: Params) async throws(ProviderError) -> Result {
         do {
+            let state = signpostBeginRequest()
+
+            defer {
+                signpostEndRequest(state: state)
+            }
+
+            logger?.info("\(params.debugDescription)")
             let (data, response) = try await urlSession.data(for: params)
+            logger?.info("\(response.debugDescription)")
+            logger?.debug("\(String(decoding: data, as: UTF8.self))")
 
             let status = response.status
-
             switch status.kind {
             case .informational, .successful, .redirection:
                 break
             case .invalid, .clientError, .serverError:
-                throw DataProviderError(status: status, data: data)
+                let error = DataProviderError(status: status, data: data)
+                logger?.error("\(error.localizedDescription)")
+                throw error
             }
 
             return (data, response)
 
         } catch {
+            logger?.error("\(error.localizedDescription)")
             throw DataProviderError(error: error)
         }
+    }
+}
+
+private
+extension URLSessionProvider {
+    func signpostBeginRequest() -> OSSignpostIntervalState? {
+        guard let signposter else { return nil }
+        let signpostID = signposter.makeSignpostID()
+        return signposter.beginInterval("Fetch request", id: signpostID)
+    }
+
+    func signpostEndRequest(state: OSSignpostIntervalState?) {
+        guard let signposter, let state else { return }
+        signposter.emitEvent("Fetch complete.")
+        signposter.endInterval("Fetch request", state)
     }
 }
